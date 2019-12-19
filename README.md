02c3894738b4d3d6f40122400dc808f4002712ac
astro <aatif@kth.se>
2019-12-19 02:55:40 +0100

    Update README.MD

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IK2206
+# IK2206 Internet Security and Privacy
 ![Assignmentpt1](/assignment_instructions/1.png)
 ![Assignmentpt2](/assignment_instructions/2.png)
 ![Assignmentpt3](/assignment_instructions/3.png)
--- src/ForwardClientLaunch.bat
+++ src/ForwardClientLaunch.bat
@@ -2,5 +2,5 @@ ECHO OFF
 cls
 ECHO RUNNING NAKOV FORWARD Client
 javac ForwardClient.java
-java ForwardClient --handshakehost=localhost --handshakeport=2206 --targethost=server.kth.se --targetport=6789 --usercert=client.pem --cacert=ca.pem --key=client-private.der
+java ForwardClient --handshakehost=localhost --handshakeport=2206 --targethost=localhost --targetport=6789 --usercert=client.pem --cacert=ca.pem --key=client-private.der
 PAUSE
